029d69ab93b90bda99d11c862d7d2dc1bc9d37c8
Miguel Rodriguez <mirdor.dev@gmail.com>
2022-08-20 16:41:44 -0500

    Setup: architecture and libraries setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Grow } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import type { AppProps } from 'next/app';
+import { SnackbarProvider } from 'notistack';
+import { customTheme } from 'themes/custom-theme';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={customTheme}>
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        autoHideDuration={2000}
+        TransitionComponent={Grow}
+      >
+        <Component {...pageProps} />
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
